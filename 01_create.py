4913cbd09d0bcd9d861682dc05d520f7e516bdcf
bosco <boscovn@protonmail.com>
2020-10-02 19:51:43 +0200

    update formatting

--- 01_create.py
+++ 01_create.py
@@ -2,8 +2,8 @@ import rx
 
 def generador(o, s):
     o.on_next('Hola')
-    o.on_next('Adios')
+    o.on_next('Adiós')
 
 observable = rx.create(generador)
 
-observable.subscribe(on_next=lambda v: print(v))
+observable.subscribe(on_next=lambda v: print(f'Recibido: {v}'))
