2aaa5a7b25661d359b7963f2e3ab8564a57c7749
bosco <boscovn@protonmail.com>
2020-10-09 19:48:05 +0200

    Filtered only movies

--- 06_omdb.py
+++ 06_omdb.py
@@ -1,5 +1,5 @@
 import requests
-import rx
+from rx import from_, operators
 from printer import Printer
 params = {
     'apikey': 'd3f6dabf',
@@ -9,7 +9,6 @@ params = {
 content = requests.get(f'http://www.omdbapi.com/', params=params)
 
 data = content.json()
-#todo ejercicio
-rx.from_(data['Search']).pipe(
-
+from_(data['Search']).pipe(
+    operators.filter(lambda t: t['Type'] == 'movie')
 ).subscribe(Printer())
