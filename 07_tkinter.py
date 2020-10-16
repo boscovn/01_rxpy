ae3a53c340fd7a23b189ac4c66df2fc9b5689ba3
bosco <boscovn@protonmail.com>
2020-10-16 20:36:52 +0200

    Calculator

--- 07_tkinter.py
+++ 07_tkinter.py
@@ -15,21 +15,28 @@ class App:
         self.text = Label(text='Esto sale más pequeño', font=('Arial', 24))
         self.text.grid(column=1, row=0)
 
-        Button(text='Pulsa', command=lambda: self.button()).grid(column=1, row=1)
+        Button(text='Pulsa', width=35, command=self.buttonCount, padx=15).grid(column=0, row=1, columnspan=2)
 
         self.entry = Entry()
         self.entry.grid(column=0, row=2)
-        Button(text='Pulsa').grid(column=1, row=2)
+        Button(text='Pulsa', command=self.buttonEntry).grid(column=1, row=2)
 
         self.combo = Combobox(values=['Cero', 'Uno', 'Dos', 'Tres', 'Cuatro'], state='readonly')
         self.combo.grid(column=0, row=3)
         self.combo.current(0)
-        Button(text='Pulsa').grid(column=1, row=3)
+        Button(text='Pulsa', command=self.buttonCombo).grid(column=1, row=3)
 
         self.window.mainloop()
 
-    def button(self):
+    def buttonCount(self):
         self.__contador += 1
+        self.text.configure(text=f'Has pulsado {self.__contador} veces')
+
+    def buttonEntry(self):
+        self.text.configure(text=f'El texto contiene {self.entry.get()}')
+
+    def buttonCombo(self):
+        self.text.configure(text=f'El índice seleccionado es {self.combo.current()}')
 
 if __name__ == '__main__':
     App()
