a8afaf4ebe7847badcbaac94c229ef6536eb92a8
bosco <boscovn@protonmail.com>
2020-10-16 20:05:52 +0200

    Tkinter app copy pasted pastebin

--- 07_tkinter.py
+++ 07_tkinter.py
@@ -7,9 +7,12 @@ class App:
         self.__contador = 0
         self.window = Tk()
         self.window.title = 'Aplicación Gráfica!'
+        # window.maxsize(width=300, height=200)
+        # window.minsize(width=100, height=120)
+        # window.geometry('800x400')
 
-        Label(text='Hola', font=('Arial', 36)).grid(column=0, row=0)
-        self.text = Label(text='Esto sale más pequeño', font=('Arial', 24))\
+        Label(text='Hola!', font=('Arial Bold', 36)).grid(column=0, row=0)
+        self.text = Label(text='Esto sale más pequeño', font=('Arial', 24))
         self.text.grid(column=1, row=0)
 
         Button(text='Pulsa', command=lambda: self.button()).grid(column=1, row=1)
@@ -17,9 +20,13 @@ class App:
         self.entry = Entry()
         self.entry.grid(column=0, row=2)
         Button(text='Pulsa').grid(column=1, row=2)
-        combo = Combobox(values=['Cero', 'Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'])
-        combo.current(0)
-        window.mainloop()
+
+        self.combo = Combobox(values=['Cero', 'Uno', 'Dos', 'Tres', 'Cuatro'], state='readonly')
+        self.combo.grid(column=0, row=3)
+        self.combo.current(0)
+        Button(text='Pulsa').grid(column=1, row=3)
+
+        self.window.mainloop()
 
     def button(self):
         self.__contador += 1
